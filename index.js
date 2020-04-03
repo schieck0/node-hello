3d8b8362c9073e592010ec6655e8bf00b9343e50
Rodrigo <rodrigo@nuvoni.com.br>
2020-04-02 21:21:28 -0300

    change databese remote

--- index.js
+++ index.js
@@ -3,36 +3,36 @@ const app = express()
 const bodyParser = require('body-parser')
 const mysql = require('mysql')
 
-// const con = mysql.createConnection({
-//     host: 'localhost',
-//     port: 3306,
-//     user: 'root',
-//     password: '123456',
-//     database: 'hello'
-// })
+const con = mysql.createConnection({
+    host: 'remotemysql.com',
+    port: 3306,
+    user: 'FZxTxHaBIt',
+    password: '7Ol1cIIFDQ',
+    database: 'FZxTxHaBIt'
+})
 
-// con.connect(function (err) {
-//     if (err) throw err;
-//     console.log("DB Connected!");
-// });
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("DB Connected!");
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-    res.send('Hello World!!!')
-    // con.query('select * from names', function (err, result) {
-    //     if (err) throw err;
-    //     res.json(result)
-    // });
+    con.query('select * from names', function (err, result) {
+        if (err) throw err;
+        res.json(result)
+    });
 })
 
-// app.post('/', function (req, res) {
-//     con.query('insert into names(name) values(?)', [req.body.name], function (err, result) {
-//         res.send("Inserted id " + result.insertId)
-//     });
-// })
+app.post('/', function (req, res) {
+    con.query('insert into names(name) values(?)', [req.body.name], function (err, result) {
+        if (err) throw err;
+        res.send("Inserted id " + result.insertId)
+    });
+})
 
 app.listen(process.env.PORT || 80, () => {
-    console.log(`Server listen on ${process.env.PORT || 80}`);
+    console.log(`Server listen on ${process.env.PORT}`);
 })
